30b015244267e9287ce0c53a10b99aa4a4feae25
Pascal Martineau <pascal@lewebsimple.ca>
2024-04-25 11:58:27 -0400

    feat: PasswordInput

---
+++ app/components/input/PasswordInput.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const password = defineModel<string>({ required: true });
+const showPassword = ref(false);
+</script>
+
+<template>
+  <UInput v-model="password" :type="showPassword ? 'text' : 'password'" :ui="{ icon: { trailing: { pointer: '' } } }">
+    <template #trailing>
+      <UButton color="gray" variant="ghost" :icon="showPassword ? 'i-heroicons-eye-slash' : 'i-heroicons-eye'" :padded="false" @click="showPassword = !showPassword" />
+    </template>
+  </UInput>
+</template>
